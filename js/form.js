137507d36a71aaeb786d97ceca12e8e9923a65a7
Anas <anas@istikhor.com>
2020-09-16 08:05:40 +0800

    update file.

---
+++ js/form.js
@@ -0,0 +1,23 @@
+//form script
+
+var inputName = document.getElementById('input-name');
+var inputMail = document.getElementById('input-email');
+var inputSubject = document.getElementById('input-subject');
+var inputMessage = document.getElementById('input-message');
+var button = document.querySelector('.submit-button');
+var regEx = /\S+@\S+\.\S+/;
+var form = document.querySelector('.form')
+
+function checkInput() {
+    if (inputName.value.trim() !== "" && regEx.test(inputMail.value) && inputSubject.value.trim() !== "" && inputMessage.value.trim() !== "") {
+        
+    button.disabled = false;
+    } else {    
+    button.disabled = true;
+    }
+}
+form.addEventListener('submit', function(event) {
+    form.submit();   
+})
+
+// /form script
