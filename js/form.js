f7e95adec1fc72beb2c726132c99a10dec87ce9e
Anas <anas@istikhor.com>
2020-10-12 09:45:35 +0800

    remove hr border at footer

--- js/form.js
+++ js/form.js
@@ -1,23 +1,27 @@
 //form script
 
-var inputName = document.getElementById('input-name');
-var inputMail = document.getElementById('input-email');
-var inputSubject = document.getElementById('input-subject');
-var inputMessage = document.getElementById('input-message');
-var button = document.querySelector('.submit-button');
+var inputName = document.getElementById("input-name");
+var inputMail = document.getElementById("input-email");
+var inputSubject = document.getElementById("input-subject");
+var inputMessage = document.getElementById("input-message");
+var button = document.querySelector(".submit-button");
 var regEx = /\S+@\S+\.\S+/;
-var form = document.querySelector('.form')
+var form = document.querySelector(".form");
 
 function checkInput() {
-    if (inputName.value.trim() !== "" && regEx.test(inputMail.value) && inputSubject.value.trim() !== "" && inputMessage.value.trim() !== "") {
-        
+  if (
+    inputName.value.trim() !== "" &&
+    regEx.test(inputMail.value) &&
+    inputSubject.value.trim() !== "" &&
+    inputMessage.value.trim() !== ""
+  ) {
     button.disabled = false;
-    } else {    
+  } else {
     button.disabled = true;
-    }
+  }
 }
-form.addEventListener('submit', function(event) {
-    form.submit();   
-})
+form.addEventListener("submit", function (event) {
+  form.submit();
+});
 
 // /form script
