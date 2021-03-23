e1778ee3960348aea755aa85acfa97de81223bc2
Anas <anas@istikhor.com>
2021-03-23 09:31:56 +0800

    update file.

--- js/messenger.js
+++ js/messenger.js
@@ -1,105 +1,105 @@
 var Messenger = function (el) {
-  "use strict";
-  var m = this;
+  'use strict'
+  var m = this
 
   m.init = function () {
-    m.codeletters = "0101010101";
-    m.message = 0;
-    m.current_length = 0;
-    m.fadeBuffer = false;
+    m.codeletters = '0101010101'
+    m.message = 0
+    m.current_length = 0
+    m.fadeBuffer = false
     m.messages = [
-      "Hi! My name is Anas.",
-      "I am a Full Stack Developer.",
-      "and",
-      "a WordPress & Shopify Expert,",
-      "also",
-      "an SEO Specialist.",
-      "Need Help?",
-      "Let&rsquo;s discuss your needs in detail.",
-      "Connect with me today.",
-    ];
+      'Hi! My name is Anas.',
+      // "I am a Full Stack Developer.",
+      // "and",
+      // "a WordPress & Shopify Expert,",
+      // "also",
+      // "an SEO Specialist.",
+      'Need Help?',
+      'Let&rsquo;s discuss your needs in detail.',
+      'Connect with me today.',
+    ]
 
-    setTimeout(m.animateIn, 100);
-  };
+    setTimeout(m.animateIn, 100)
+  }
 
   m.generateRandomString = function (length) {
-    var random_text = "";
+    var random_text = ''
     while (random_text.length < length) {
       random_text += m.codeletters.charAt(
         Math.floor(Math.random() * m.codeletters.length)
-      );
+      )
     }
 
-    return random_text;
-  };
+    return random_text
+  }
 
   m.animateIn = function () {
     if (m.current_length < m.messages[m.message].length) {
-      m.current_length = m.current_length + 2;
+      m.current_length = m.current_length + 2
       if (m.current_length > m.messages[m.message].length) {
-        m.current_length = m.messages[m.message].length;
+        m.current_length = m.messages[m.message].length
       }
 
-      var message = m.generateRandomString(m.current_length);
-      $(el).html(message);
+      var message = m.generateRandomString(m.current_length)
+      $(el).html(message)
 
-      setTimeout(m.animateIn, 20);
+      setTimeout(m.animateIn, 20)
     } else {
-      setTimeout(m.animateFadeBuffer, 20);
+      setTimeout(m.animateFadeBuffer, 20)
     }
-  };
+  }
 
   m.animateFadeBuffer = function () {
     if (m.fadeBuffer === false) {
-      m.fadeBuffer = [];
+      m.fadeBuffer = []
       for (var i = 0; i < m.messages[m.message].length; i++) {
         m.fadeBuffer.push({
           c: Math.floor(Math.random() * 12) + 1,
           l: m.messages[m.message].charAt(i),
-        });
+        })
       }
     }
 
-    var do_cycles = false;
-    var message = "";
+    var do_cycles = false
+    var message = ''
 
     for (var i = 0; i < m.fadeBuffer.length; i++) {
-      var fader = m.fadeBuffer[i];
+      var fader = m.fadeBuffer[i]
       if (fader.c > 0) {
-        do_cycles = true;
-        fader.c--;
+        do_cycles = true
+        fader.c--
         message += m.codeletters.charAt(
           Math.floor(Math.random() * m.codeletters.length)
-        );
+        )
       } else {
-        message += fader.l;
+        message += fader.l
       }
     }
 
-    $(el).html(message);
+    $(el).html(message)
 
     if (do_cycles === true) {
-      setTimeout(m.animateFadeBuffer, 50);
+      setTimeout(m.animateFadeBuffer, 50)
     } else {
-      setTimeout(m.cycleText, 2000);
+      setTimeout(m.cycleText, 2000)
     }
-  };
+  }
 
   m.cycleText = function () {
-    m.message = m.message + 1;
+    m.message = m.message + 1
     if (m.message >= m.messages.length) {
-      m.message = 0;
+      m.message = 0
     }
 
-    m.current_length = 0;
-    m.fadeBuffer = false;
-    $(el).html("");
+    m.current_length = 0
+    m.fadeBuffer = false
+    $(el).html('')
 
-    setTimeout(m.animateIn, 200);
-  };
+    setTimeout(m.animateIn, 200)
+  }
 
-  m.init();
-};
+  m.init()
+}
 
-console.clear();
-var messenger = new Messenger($("#messenger"));
+console.clear()
+var messenger = new Messenger($('#messenger'))
